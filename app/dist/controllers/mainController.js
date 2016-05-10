11be77c2a46f607fcd65f3400ce04eda6d6dbf15
hogvarce <hogvarce@gmail.com>
2016-05-10 20:45:44 +0300

    first commit

---
+++ app/dist/controllers/mainController.js
@@ -0,0 +1,84 @@
+/// <reference path="../_all.ts"/>
+var ContactManagerApp;
+(function (ContactManagerApp) {
+    var MainController = (function () {
+        function MainController(userService, $mdSidenav, $mdToast, $mdDialog, $mdMedia) {
+            this.userService = userService;
+            this.$mdSidenav = $mdSidenav;
+            this.$mdToast = $mdToast;
+            this.$mdDialog = $mdDialog;
+            this.$mdMedia = $mdMedia;
+            this.tabIndex = 0;
+            this.searchText = "";
+            this.users = [];
+            this.selected = null;
+            this.message = "Hello from out controller";
+            var self = this;
+            this.userService
+                .loadAllUsers()
+                .then(function (users) {
+                self.users = users;
+                self.selected = users[0];
+                self.userService.selectedUser = self.selected;
+                console.log(self.users);
+            });
+        }
+        MainController.prototype.toogleSideNav = function () {
+            this.$mdSidenav('left').toggle();
+        };
+        MainController.prototype.selectUser = function (user) {
+            this.selected = user;
+            this.userService.selectedUser = user;
+            var sidenav = this.$mdSidenav('left');
+            if (sidenav.isOpen()) {
+                sidenav.close();
+            }
+            this.tabIndex = 0;
+        };
+        MainController.prototype.addUser = function ($event) {
+            var self = this;
+            var userFullScreen = (this.$mdMedia('sm') || this.$mdMedia('xs'));
+            this.$mdDialog.show({
+                templateUrl: './dist/view/newUserDialog.html',
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                controller: ContactManagerApp.AddUserDialogController,
+                controllerAs: 'ctrl',
+                clickOutsideToClose: true,
+                fullscreen: userFullScreen
+            }).then(function (user) {
+                self.openToast("User added");
+            }, function () {
+                console.log('You cancelled the dialig.');
+            });
+        };
+        MainController.prototype.clearNotes = function ($event) {
+            var confirm = this.$mdDialog.confirm()
+                .title('Are you sure you want to deleter all notes?')
+                .textContent('All notes will be deleted, you cannot undo this action')
+                .targetEvent($event)
+                .ok('Yes')
+                .cancel('No');
+            var self = this;
+            this.$mdDialog.show(confirm).then(function () {
+                self.selected.notes = [];
+                self.openToast('clear Notes');
+            });
+        };
+        MainController.prototype.removeNote = function (note) {
+            var foundIndex = this.selected.notes.indexOf(note);
+            this.selected.notes.splice(foundIndex, 1);
+            this.openToast("Notes has removed!");
+        };
+        MainController.prototype.openToast = function (message) {
+            this.$mdToast.show(this.$mdToast.simple()
+                .textContent(message)
+                .position('top right')
+                .hideDelay(3000));
+        };
+        MainController.$inject = ['userService', '$mdSidenav', '$mdToast', '$mdDialog', '$mdMedia'];
+        return MainController;
+    }());
+    ContactManagerApp.MainController = MainController;
+})(ContactManagerApp || (ContactManagerApp = {}));
+//# sourceMappingURL=mainController.js.map
---
+++ app/src/controllers/mainController.ts
@@ -0,0 +1,95 @@
+/// <reference path="../_all.ts"/>
+
+module ContactManagerApp{
+    export class MainController {
+        static $inject = ['userService', '$mdSidenav', '$mdToast', '$mdDialog', '$mdMedia'];
+        constructor(
+             private userService: IUserService,
+             private $mdSidenav: angular.material.ISidenavService,
+             private $mdToast: angular.material.IToastService,
+             private $mdDialog: angular.material.IDialogService,
+             private $mdMedia: angular.material.IMedia
+        ){
+            var self= this;
+
+            this.userService
+                .loadAllUsers()
+                .then((users: User[]) => {
+                  self.users = users;
+                  self.selected = users[0];
+                  self.userService.selectedUser = self.selected;
+
+                  console.log(self.users);
+                });
+        }
+
+        tabIndex: number = 0;
+        searchText: string = "";
+        users: User[] = [];
+        selected: User = null;
+        message: string = "Hello from out controller";
+
+        toogleSideNav(): void {
+            this.$mdSidenav('left').toggle();
+        }
+        selectUser (user: User) : void {
+            this.selected = user;
+            this.userService.selectedUser = user;
+
+            var sidenav = this.$mdSidenav('left');
+            if (sidenav.isOpen()) {
+                sidenav.close();
+            }
+
+            this.tabIndex = 0;
+        }
+
+        addUser ($event) {
+            var self = this;
+            var userFullScreen = (this.$mdMedia('sm') || this.$mdMedia('xs'));
+
+            this.$mdDialog.show({
+                templateUrl: './dist/view/newUserDialog.html',
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                controller: AddUserDialogController,
+                controllerAs: 'ctrl',
+                clickOutsideToClose: true,
+                fullscreen: userFullScreen
+            }).then((user: User)=>{
+                self.openToast("User added");
+            }, () => {
+                console.log('You cancelled the dialig.');
+            });
+        }
+
+        clearNotes($event) {
+            var confirm = this.$mdDialog.confirm()
+                .title('Are you sure you want to deleter all notes?')
+                .textContent('All notes will be deleted, you cannot undo this action')
+                .targetEvent($event)
+                .ok('Yes')
+                .cancel('No');
+            var self = this;
+            this.$mdDialog.show(confirm).then(() => {
+                self.selected.notes = [];
+                self.openToast('clear Notes');
+            })
+        }
+
+        removeNote(note: Note): void {
+            var foundIndex = this.selected.notes.indexOf(note);
+            this.selected.notes.splice(foundIndex, 1);
+            this.openToast("Notes has removed!");
+        }
+
+        openToast(message: string): void {
+            this.$mdToast.show(
+                this.$mdToast.simple()
+                .textContent(message)
+                .position('top right')
+                .hideDelay(3000)
+            )
+        }
+    }
+}
